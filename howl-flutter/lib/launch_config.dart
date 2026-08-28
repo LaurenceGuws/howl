06c609b5f04399b6c4dd6a9c3b5e58f7c800e0c6
Laurence <lggousie46@gmail.com>
2026-08-28 09:30:42 +0200

    delete slop

--- howl-flutter/lib/launch_config.dart
+++
@@ -1,7 +0,0 @@
-bool geometryLeaderEnabled({
-  required String compiledValue,
-  String? environmentValue,
-}) {
-  final value = compiledValue.isNotEmpty ? compiledValue : environmentValue;
-  return value == '1';
-}
